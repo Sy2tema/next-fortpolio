6c9c56a51bc46fe386e07ce196c9469fba090bd1
GunHyuk Lee <bood00142@gmail.com>
2022-06-26 17:52:18 +0900

    Fix '404.js'
    Resolves: return부분을 작성

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,7 @@
-export default function 404 () {
-    <>
-        <div>요청하신 페이지를 찾을 수 없습니다.</div>
-    </>
+export default function PageNotFound () {
+    return (
+        <>
+            <div>요청하신 페이지를 찾을 수 없습니다.</div>
+        </>
+    );
 }
