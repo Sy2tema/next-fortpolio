c8aea58f35772f27064927dca072371f0e939d8e
GunHyuk Lee <bood00142@gmail.com>
2022-07-10 18:43:38 +0900

    Add a tag "_blank" option
    Link태그 부분에서 target옵션을 적용하면 제대로 새탭으로 열리지 않았음.
    a태그 부분에 target옵션을 적용하니 정상적으로 새탭에서 링크가 열리는것을 확인.

--- components/header.js
+++ components/header.js
@@ -21,7 +21,7 @@ export default function Header () {
                             <a className="mr-5 hover:text-gray-900">프로젝트</a>
                         </Link>
                         <Link href='https://github.com/Sy2tema'>
-                            <a className="mr-5 hover:text-gray-900">깃허브</a>
+                            <a className="mr-5 hover:text-gray-900" target="_blank">깃허브</a>
                         </Link>
                     </nav>
                     <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
