d8a162a064a51562adeb38b0a48f18958a8e90e2
GunHyuk Lee <bood00142@gmail.com>
2022-07-10 22:43:39 +0900

    :art: Modify CSS style

--- components/home/hero.js
+++ components/home/hero.js
@@ -11,7 +11,7 @@ export default function Hero() {
                 <p className="mb-8 leading-relaxed">로렘 입숨(lorem ipsum; 줄여서 립숨, lipsum)은 출판이나 그래픽 디자인 분야에서 폰트, 타이포그래피, 레이아웃 같은 그래픽 요소나 시각적 연출을 보여줄 때 사용하는 표준 채우기 텍스트로, 최종 결과물에 들어가는 실제적인 문장 내용이 채워지기 전에 시각 디자인 프로젝트 모형의 채움 글로도 이용된다. 이런 용도로 사용할 때 로렘 입숨을 그리킹(greeking)이라고도 부르며, 때로 로렘 입숨은 공간만 차지하는 무언가를 지칭하는 용어로도 사용된다.</p>
                 <div className="flex justify-center">
                     <Link href='/about_me'>
-                        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                        <button className="btn-project">
                             프로젝트 보러가기
                         </button>
                     </Link>
