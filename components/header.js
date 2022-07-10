37658ce54bebae4569f466728c88b58eb7061456
GunHyuk Lee <bood00142@gmail.com>
2022-07-10 21:10:32 +0900

    :lipstick: Add dark-mode theme

--- components/header.js
+++ components/header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import DarkModeToggleButton from './dark_mode_toggle_button';
 
 export default function Header () {
     return (
@@ -24,8 +25,7 @@ export default function Header () {
                             <a className="mr-5 hover:text-gray-900" target="_blank">깃허브</a>
                         </Link>
                     </nav>
-                    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-                    </button>
+                    <DarkModeToggleButton />
                 </div>
             </header>
         </>
